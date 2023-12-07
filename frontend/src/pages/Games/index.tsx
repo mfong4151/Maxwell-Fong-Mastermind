75235568c94085b8792b02fc1dc2e85fb42399c1
Max <mfong415@gmail.com>
2023-12-06 17:17:07 -0800

    Added hints

--- frontend/src/pages/Games/index.tsx
+++ frontend/src/pages/Games/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useErrors } from '../../hooks';
 import Errors from '../../components/Errors';
 import MyGames from './MyGames';
 import NewGameForm from '../../components/NewGameForm';
+import { useNavigate } from 'react-router-dom';
+import { isLoggedIn } from '../../utils';
 
 const Games: React.FC = () => {
+  const navigate = useNavigate()
+  useEffect(()=> {
 
+    if(!isLoggedIn()){
+    navigate('/login')
+  }
+  })
   return (
     <div>
       <div className='flex-between'>
